f648f0804c75c8477479b4531a9de84c3fec7fa5
Vivek <vivekkv178@gmail.com>
2024-03-16 06:56:22 +0800

    Iframe Test

--- components/Storybook.jsx
+++ components/Storybook.jsx
@@ -4,14 +4,14 @@ const Storybook = () => {
   return (
     <section id="storybook" className="px-8 mx-4">
       <SectionTitle title="Storybook" />
-      <div className="h-screen mb-20 border border-black border-double rounded-lg">
-        <iframe
-          src={process?.env?.NEXT_PUBLIC_STORYBOOK_URL}
-          width="100%"
-          height="100%"
-          className="rounded-lg"
-        ></iframe>
-      </div>
+      {/* <div className="h-screen mb-20 border border-black border-double rounded-lg"> */}
+      <iframe
+        src={process?.env?.NEXT_PUBLIC_STORYBOOK_URL}
+        // width="100%"
+        // height="100%"
+        className="rounded-lg"
+      ></iframe>
+      {/* </div> */}
     </section>
   );
 };

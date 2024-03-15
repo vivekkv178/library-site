723815d2e4ca0cc84e000e488a9808e1a2079781
Vivek <vivekkv178@gmail.com>
2024-03-16 07:21:03 +0800

    Mobile test

--- components/Storybook.jsx
+++ components/Storybook.jsx
@@ -7,9 +7,7 @@ const Storybook = () => {
       <div className="h-screen mb-20 border border-black border-double rounded-lg">
         <iframe
           // src={process?.env?.NEXT_PUBLIC_STORYBOOK_URL}
-          src={
-            "https://5ccbc373887ca40020446347-bysekhynzd.chromatic.com/?embed=true&full=1&path=%2Fstory%2Fshadowboxcta--default&referrer=https%3A%2F%2Fmedium.com%2F%40ghengeveld%2Fembedding-storybook-on-medium-ce8a280c03ad&shortcuts=false&singleStory=true"
-          }
+          src={"https://5ccbc373887ca40020446347-bysekhynzd.chromatic.com"}
           width="100%"
           height="100%"
           className="rounded-lg"

2559d79416566ed279388a8665e5b79ed1110346
Vivek <vivekkv178@gmail.com>
2024-03-22 21:15:04 +0800

    Mobile test

--- components/Storybook.jsx
+++ components/Storybook.jsx
@@ -6,8 +6,10 @@ const Storybook = () => {
       <SectionTitle title="Storybook" />
       <div className="h-screen mb-20 border border-black border-double rounded-lg">
         <iframe
-          src={process?.env?.NEXT_PUBLIC_STORYBOOK_URL}
-          // src={"https://5ccbc373887ca40020446347-bysekhynzd.chromatic.com"}
+          // src={process?.env?.NEXT_PUBLIC_STORYBOOK_URL}
+          src={
+            "https://5ccbc373887ca40020446347-bysekhynzd.chromatic.com/?path=/story/shadowboxcta--default"
+          }
           width="100%"
           height="100%"
           className="rounded-lg"
